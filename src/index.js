079fa3acb9de8edc91d9d93bec4c16ee44d22631
Richard McCartney <richard@mccartney.io>
2018-08-02 13:32:28 +0100

    Setting up eslint and commands.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const title = "My Minimal React Webpack Babel Setup";
+const title = 'My Minimal React Webpack Babel Setup'
 
-ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
+ReactDOM.render(<div>{title}</div>, document.getElementById('app'))
 
-module.hot.accept();
+module.hot.accept()
