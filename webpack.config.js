26843f38050a8f25f86bae41e8fa2e5462bb3a2e
Richard McCartney <richard@mccartney.io>
2018-08-02 13:49:12 +0100

    Removing and moving dist content.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-const webpack = require("webpack");
+const webpack = require('webpack')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
-    ]
+        use: ['babel-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"]
+    extensions: ['*', '.js', '.jsx'],
   },
   output: {
-    path: __dirname + "/dist",
-    publicPath: "/",
-    filename: "bundle.js"
+    path: __dirname + '/src',
+    publicPath: '/',
+    filename: 'app.js',
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
-    contentBase: "./dist",
-    hot: true
-  }
-};
+    contentBase: './src',
+    hot: true,
+  },
+}
