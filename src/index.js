8e90b7225cfc13c5bb64dc6542da335730caf91e
Richard McCartney <richard@mccartney.io>
2018-08-02 12:55:08 +0100

    Setting up 🐠  babel and webpack reloading.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "My Minimal React Webpack Babel Setup";
+
+ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
+
+module.hot.accept();
