9a32318ccbad0d255742fbde6fb9c84b9bdac171
Richard McCartney <richard.mccartney@ynap.com>
2019-05-24 15:13:35 +0200

    Structure clean up.

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import Reducers from './reducers'
+import Reducers from './state/reducers'
 
 const store = createStore(Reducers, applyMiddleware(thunk))
 
